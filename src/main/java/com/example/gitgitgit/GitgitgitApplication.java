51852acf26b9cb388dd4bde59a4950d2710dab1f
Kim Suyong <rlaghkdyd99@naver.com>
2023-10-23 14:31:02 +0900

    Initial commit

---
+++ src/main/java/com/example/gitgitgit/GitgitgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitgitgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgitgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitgitgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitgitgit/GitgitgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitgitgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitgitgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
